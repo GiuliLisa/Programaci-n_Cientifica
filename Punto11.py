d577183fd26b53bd28d4917c50bebfbeb4c5b5d2
GiuliLisa <giuliannaevelin@gmail.com>
2025-04-11 22:59:33 -0300

    Cambios finales

--- Punto11.py
+++ Punto11.py
@@ -1,9 +1,9 @@
-#
+#11 Aplicar la operación de suma a las variables declaradas en los puntos 8 y 9 y ver qué ocurre. 
 
-#(punto 8)
+#(punto 8 aplicando suma)
 print(2 ** 5 + 1) 
 
-#(punto 9)
+#(punto 9 aplicando suma)
 nombre = "Giuli" + 5
 print(nombre)
 
--- Punto12 (nombres).py
+++ Punto12 (nombres).py
@@ -1,3 +1,5 @@
+#12  Con el VS Code, crear un módulo que se llame nombres.py. En él, crear dos variables, una llamada nombre y la otra llamada apellido que deberán tener los valores correspondientes a tu primer nombre y apellido correspondientemente. 
+
 nombre = "Giulianna"
 apellido = "Reinaldo"
 print(nombre, apellido)
