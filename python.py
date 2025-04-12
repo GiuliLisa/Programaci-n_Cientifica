89b823a7c3b9aa439507e719dac6862385a10558
GiuliLisa <giuliannaevelin@gmail.com>
2025-04-11 21:04:40 -0300

    Hice todos los ejercicios

---
+++ python.py
@@ -0,0 +1,52 @@
+#1
+print("hola mundo")
+
+#2
+hola_mundo = "Hola Mundo!"
+print(hola_mundo)
+
+#3
+a = (3 + 2) / 2.5
+print(a)
+
+#4
+387 - 124
+
+#5
+print("Giulianna")
+
+#6
+print("Gato")
+
+#7
+#Crea un programa que defina dos números enteros, realiza la suma y la multiplicación entre ellos, y muestra ambos resultados por pantalla con un mensaje descriptivo.
+
+#8 
+print(2 ** 5 + 1)
+
+#9
+nombre = "Giuli" #+ 5    le pongo asi pq sino no me toma, pero igual tira error por la variable string y la variable numerica, no se pueden juntar
+print(nombre)
+
+#10
+apellido = "Reinaldo"
+print(apellido)
+
+#11
+#ya está
+
+#12 ya está
+
+#13
+nombre = input("ingresa tu nombre por favor: ")   #la funcion input permite ingresar datos
+print("hola", nombre)
+
+#14
+horas = int(input("Ingrese el número de horas trabajadas: "))
+coste = float(input("Ingrese el coste por hora: "))
+cobro = horas * coste
+print(cobro)
+
+#cuando se trata de numeros, hay que convertir el texto de input, por ende se pone
+#int() o float() antes de input, para que el valor de texto pase a numero.
+#si solo se pone int, el programa lo toma como una variable de texto.
