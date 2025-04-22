74569aa11667e0423a9e7836e78a54628c2d3700
GiuliLisa <giuliannaevelin@gmail.com>
2025-04-22 19:55:24 -0300

    Finalizado

--- TP 2/Punto27.py
+++ TP 2/Punto27.py
@@ -3,6 +3,6 @@
 #Programación Científica
 
 #27. Calcula el perímetro y área de un cuadrado (4*lado y lado²) usando asignación múltiple.
-lado = int(input("Ingrese la longitud del lado: "))
-perimetro, area = 10 * 4, 5 ** 2
+lado = float(input("Ingrese la longitud del lado: "))
+perimetro, area = 4 * lado, lado ** 2
 print(f"Area: {area}, Perimetro: {perimetro}")
