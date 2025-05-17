2dbcb373bca55d83d9d3dc8834698ebf7241be99
GiuliLisa <giuliannaevelin@gmail.com>
2025-05-17 15:52:47 -0300

    Eliminar TP2-a y mantener solo TP2-b

--- TP2-a/Punto17.py
+++
@@ -1,9 +0,0 @@
-#Reinaldo Giulianna
-#2025
-#Programación Científica
-
-#17. Crea una tupla con un solo elemento 50 
-
-tupla = (50,)
-
-print(tupla)
--- TP2-a/Punto7.py
+++
@@ -1,9 +0,0 @@
-#Reinaldo Giulianna
-#2025
-#Programación Científica
-
-#7. En base a la lista del punto anterior mostrar por pantalla la materia favorita.
-
-asignaturas = ["Matemáticas", "Física", "Química", "Programacion Cientifica", "Practica Profesional"]
-
-print("Asignatura favorita:", asignaturas[2])
