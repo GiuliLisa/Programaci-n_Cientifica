5487a6151d8543e576f598791114769c85d6c125
GiuliLisa <giuliannaevelin@gmail.com>
2025-05-25 20:04:12 -0300

    Eliminar carpeta TP2-b de la rama Unidad-3-a

--- TP2-b/Punto17.py
+++
@@ -1,8 +0,0 @@
-#Reinaldo Giulianna
-#2025
-#Programación Científica
-
-# 17. Utilizando una estructura de repetición, escribir un programa que cree un bucle infinito. 
-
-while True:
-    print("Este mensaje se repite para siempre...")
