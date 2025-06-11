0df6931946d67cdc59d326acebce67c5b44b617b
GiuliLisa <giuliannaevelin@gmail.com>
2025-06-11 13:26:51 -0300

    Borrar carpeta TP3-a de la rama Unidad-2

--- TP3-a/calculos.py
+++
@@ -1,6 +0,0 @@
-import operaciones
-
-print(f"Suma: {operaciones.suma(5, 3)}")
-print(f"Resta: {operaciones.resta(10, 5)}")
-print(f"Multiplicación: {operaciones.multi(5, 3)}")
-print(f"División: {operaciones.div(10, 0)}")
--- TP3-a/nombres.py
+++
@@ -1,9 +0,0 @@
-#Reinaldo Giulianna
-#2025
-#Programación Científica
-
-# 5. Crear un módulo que se llame nombres.py donde declares una variable con tu primer
-# nombre, otra con tu apellido.
-
-nombre = "Giulianna"
-apellido = "Reinaldo"
