e69fc7e9f783c78fd983ef6013caba0c6263a722
GiuliLisa <giuliannaevelin@gmail.com>
2025-09-21 16:09:37 -0300

    Eliminar carpeta TP3-b de la rama Unidad-4-a

--- TP3-b/Punto1.py
+++
@@ -1,8 +0,0 @@
-#Reinaldo Giulianna
-#2025
-#Programación Científica
-
-#1. Crear un archivo txt y luego escribir “Hola mundo”. 
-
-with open("datos.txt", "w") as archivo:
-    archivo.write("Hola mundo\n")
